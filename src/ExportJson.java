d1dc43d48cb4e3b3658a224dc0c8220e658ec4f9
Adatrader <adamvanriper@gmail.com>
2020-04-30 12:11:12 -0700

    Rearranged
    Anti tasks before transient tasks in JSON

--- src/ExportJson.java
+++ src/ExportJson.java
@@ -9,16 +9,16 @@ public class ExportJson
     public void export(Schedule schedulePassed)
     {
         Set<RecurringTask> recurringTasks = schedulePassed.getRecurringTasks();
-        Set<TransientTask> transientTasks = schedulePassed.getTransientTasks();
         Set<AntiTask> antiTasks = schedulePassed.getAntiTasks();
+        Set<TransientTask> transientTasks = schedulePassed.getTransientTasks();
         try
         {
             Writer writer = new FileWriter("Schedule.json");
 
             // convert taskTemp to JSON file
             new Gson().toJson(recurringTasks, writer);
-            new Gson().toJson(transientTasks, writer);
             new Gson().toJson(antiTasks, writer);
+            new Gson().toJson(transientTasks, writer);
 
             writer.close();
 
