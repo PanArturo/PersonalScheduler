3ed40781f46995894287093e94bfa9f6c734b280
htfpop <ckleung@cpp.edu>
2020-05-02 18:30:21 -0700

    Pathing for set1 and set2 need to be configured through program arguments

--- src/Test3.java
+++ src/Test3.java
@@ -4,12 +4,12 @@ public class Test3 {
 
     public static void main(String args[]){
         String set1 = args[0];
+        String set2 = args[1];
         Schedule schedule = new Schedule();
         ImportJson inTest = new ImportJson(set1);
 //        inTest.getFromUI(new File("/Users/adamv/Nextcloud/Cal Poly Documents/2020 Spring/CS 3560 -Object Oriented Design and Programming/Set1.json"));
         schedule.merge(inTest.currentSched);
         ExportJson exTest = new ExportJson();
-        //exTest.export(schedule, new File("C:/Users/adamv/Nextcloud/Cal Poly Documents/2020 Spring/CS 3560 -Object Oriented Design and Programming"));
 
 
 
